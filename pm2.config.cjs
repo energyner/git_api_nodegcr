6c6c4ce4e65a746cbaa98877e17e0b350f84e45b
RRD <info@enegyner.net>
2025-07-15 16:41:45 -0400

    9- Actualizando el repositorio local git_api_nodegcr para su despliege en Google Cloud

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,21 +3,15 @@
 module.exports = {
   apps: [
     {
-      name: "git-api-nodegcr", // Nombre de tu aplicación
-      script: "main.mjs",      // El archivo principal a ejecutar
-      interpreter: "node",     // Usar el intérprete de Node.js
-  
-      env: {
-        NODE_ENV: "production",
-        PORT: 8080 // Aquí puedes poner el puerto por defecto, pero Cloud Run lo sobreescribirá
-                   // con su variable de entorno PORT. PM2 leerá process.env.PORT.
-      },
-      instances: "max", // Escalar a la máxima cantidad de núcleos de CPU disponibles
-      exec_mode: "cluster", // Usar modo clúster para aprovechar múltiples núcleos
-      max_memory_restart: "300M", // Reiniciar si el uso de memoria supera este límite
+      name: "energyner",
+      script: "main.mjs",
+      interpreter: "node",
+      watch: false,
+      exec_mode: "fork", // Más compatible con Cloud Run
+      instances: 1,      // Una instancia por contenedor
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      error_file: "/dev/stderr", // Enviar errores al stderr para que Cloud Run los recoja
-      out_file: "/dev/stdout"    // Enviar salida estándar al stdout para que Cloud Run los recoja
+      error_file: "/dev/stderr",
+      out_file: "/dev/stdout"
     }
   ]
 };
